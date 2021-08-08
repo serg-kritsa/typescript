371edf1a21a7cc587767b6cf21801143f4ff3dd0
Serg <serg@example.com>
2021-08-09 00:07:11 +0300

    added array example

--- basics/02-object/app.ts
+++ basics/02-object/app.ts
@@ -4,7 +4,16 @@
 // } = {
 const person = {
   name: 'Maximilian',
-  age: 30
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!! inference type
+}
