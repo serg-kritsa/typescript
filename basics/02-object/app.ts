2ec7937d518b0a75ca45c92bf6592bb8334e1616
Serg <serg@example.com>
2021-08-09 00:17:56 +0300

    enum demo. not exists in js

--- basics/02-object/app.ts
+++ basics/02-object/app.ts
@@ -2,16 +2,29 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Maximilian',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
+
+const person = {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN
 };
 
 // person.role.push('admin'); // NO tuple error
@@ -26,5 +39,9 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
-  // console.log(hobby.map()); // !!! ERROR !!! inference type
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
 }
