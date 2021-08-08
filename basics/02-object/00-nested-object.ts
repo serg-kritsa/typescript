8dfb65b1a09de9bd41a9503df204eacf61ffa132
Serg <serg@example.com>
2021-08-08 23:56:35 +0300

    added object type

---
+++ basics/02-object/00-nested-object.ts
@@ -0,0 +1,18 @@
+// const product: {
+//     id: string;
+//     price: number;
+//     tags: string[];
+//     details: {
+//       title: string;
+//       description: string;
+//     }
+// } = {
+const product = {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+      title: 'Red Carpet',
+      description: 'A great carpet - almost brand-new!'
+    }
+  }
