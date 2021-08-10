986ff43d335970218848c43f35db20b068ec13b3
Serg <serg@example.com>
2021-08-11 00:28:32 +0300

    force overriding method of class by abstract keyword

--- basics/07-classes/src/app.ts
+++ basics/07-classes/src/app.ts
@@ -1,22 +1,20 @@
-class Department {
+abstract class Department {
   static fiscalYear = 2020;
   // private readonly id: string;
   // private name: string;
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
-    console.log(Department.fiscalYear);
+    // console.log(Department.fiscalYear);
   }
 
   static createEmployee(name: string) {
     return { name: name };
   }
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`);
-  }
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     // validation etc
@@ -37,6 +35,10 @@ class ITDepartment extends Department {
   constructor(id: string, admins: string[]) {
     super(id, 'IT');
     this.admins = admins;
+  }
+
+  describe() {
+    console.log('IT Department - ID: ' + this.id);
   }
 }
 
@@ -70,6 +72,10 @@ class AccountingDepartment extends Department {
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
     this.lastReport = reports[0];
+  }
+
+  describe() {
+    console.log('Accounting Department - ID: ' + this.id);
   }
 
   addEmployee(name: string) {
