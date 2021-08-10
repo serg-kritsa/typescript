c18dc192ad8bbb61ed43150be960fa9738e1b808
Serg <serg@example.com>
2021-08-10 23:55:32 +0300

    set field as protected to give access to child classes

--- basics/07-classes/src/app.ts
+++ basics/07-classes/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
