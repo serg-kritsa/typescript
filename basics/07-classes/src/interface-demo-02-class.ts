b5579126e4f18752bcc606ed77a5b62c7475cc9e
Serg <serg@example.com>
2021-08-11 00:51:35 +0300

    interface demos

---
+++ basics/07-classes/src/interface-demo-02-class.ts
@@ -0,0 +1,22 @@
+interface Greetable {
+  name: string;
+  
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name: string;
+  age = 30;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
+  }
+}
+
+let user2 = new Person('Max');
+
+console.log(user2);
