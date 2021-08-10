4f4d18058e0e4b3df193be0534bbc9f56b539847
Serg <serg@example.com>
2021-08-10 22:31:52 +0300

    added private modifier for class field

--- basics/07-classes/src/app.ts
+++ basics/07-classes/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,13 +9,30 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name);
   }
+
+  addEmployee(employee: string) {
+    // validation etc
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
 
-accounting.describe();
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 
-const accountingCopy00 = { describe: accounting.describe };
-// accountingCopy00.describe(); // error
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe(); // Department: DUMMY
+// accounting.employees[2] = 'Anna'; // error for private field
+
+accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
+
+// const accountingCopy00 = { describe: accounting.describe };
+// // accountingCopy00.describe(); // error
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe(); // Department: DUMMY
