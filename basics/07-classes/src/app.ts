42242e6bd16ddc87664a62b1166cc38b51d6ec17
Serg <serg@example.com>
2021-08-10 22:21:38 +0300

    lose of content demo

---
+++ basics/07-classes/src/app.ts
@@ -0,0 +1,18 @@
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe() {
+    console.log('Department: ' + this.name);
+  }
+}
+
+const accounting = new Department('Accounting');
+
+accounting.describe();
+
+const accountingCopy = { describe: accounting.describe };
+accountingCopy.describe(); // Department: undefined
