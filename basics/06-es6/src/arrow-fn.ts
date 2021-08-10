a36481ed70bd9c5b937f194a2c66884d82ab3d02
Serg <serg@example.com>
2021-08-10 03:04:18 +0300

    arrow fn demo

---
+++ basics/06-es6/src/arrow-fn.ts
@@ -0,0 +1,27 @@
+
+// arrow function #1
+// const fnWithReturn = function(a: number, b: number) {
+//   return a + b; 
+// }
+const fnWithReturn = (a: number, b: number) => {
+  return a + b; 
+}
+
+// arrow function #2
+const button = document.querySelector('button');
+if (button) {
+  // button.addEventListener('click', function(event){ console.log(event) });
+  button.addEventListener('click', event => console.log(event));
+}
+
+// arrow function #3
+const add = (a: number, b: number) => a + b; // shorthand for function w/ 1 return
+const printOutput: (a: number | string) => void = output => console.log(output); // declaration: definition w/ union = implementation. example below
+printOutput(add(5, 2));
+
+// function w/ union type as param
+// const fn1 = paramWithoutType => console.log(paramWithoutType); // error // type should be added
+const fn2 = (paramWithUnion: number | string) => console.log(paramWithUnion); // now it works
+// const fn3: Function = paramWithoutType => console.log(paramWithoutType); // error // type should be added
+let fn4: (a: number | string) => void; // union in definition
+fn4 = output => console.log(output);
