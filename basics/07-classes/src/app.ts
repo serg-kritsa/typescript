4f4c8643f4c2f12ebf3779a8b4f802f74aa880d0
Serg <serg@example.com>
2021-08-11 00:22:07 +0300

    added static method & property

--- basics/07-classes/src/app.ts
+++ basics/07-classes/src/app.ts
@@ -1,4 +1,5 @@
 class Department {
+  static fiscalYear = 2020;
   // private readonly id: string;
   // private name: string;
   protected employees: string[] = [];
@@ -6,6 +7,11 @@ class Department {
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
+    console.log(Department.fiscalYear);
+  }
+
+  static createEmployee(name: string) {
+    return { name: name };
   }
 
   describe(this: Department) {
@@ -22,6 +28,9 @@ class Department {
     console.log(this.employees);
   }
 }
+
+const employee1 = Department.createEmployee('Max');
+console.log(employee1, Department.fiscalYear);
 
 class ITDepartment extends Department {
   admins: string[];
