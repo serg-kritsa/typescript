9c7efc1dfa08cd939d53df00c3091244995eb722
Serg <serg@example.com>
2021-08-10 23:20:18 +0300

    class-in-es5-style

--- 00-how-to.md
+++ 00-how-to.md
@@ -53,5 +53,7 @@ see console messages in `DEBUG CONSOLE` tab not `TERMINAL`
 ## use reference value
 [https://academind.com/tutorials/reference-vs-primitive-values/]
 
-
+## write a class
+[https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Public_class_fields]
+[https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain]
 
---
+++ basics/07-classes/src/class-in-es5-style.js
@@ -0,0 +1,25 @@
+"use strict";
+var Department = (function () {
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    Department.prototype.describe = function () {
+        console.log("Department (" + this.id + "): " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    return Department;
+}());
+var accounting = new Department('d1', 'Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
