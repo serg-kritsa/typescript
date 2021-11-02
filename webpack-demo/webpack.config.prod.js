1064b78c095867f43ecda1e7bbb886ad13163219
Serg <serg@example.com>
2021-11-02 15:48:40 +0300

    added webpack prod config

---
+++ webpack-demo/webpack.config.prod.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
